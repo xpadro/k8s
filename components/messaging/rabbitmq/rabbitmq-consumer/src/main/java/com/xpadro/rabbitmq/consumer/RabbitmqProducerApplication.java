045f3543cd8e36b1f6f55db1509cb69147b18b36
Xavier Padro <xpadro.sw@gmail.com>
2024-04-20 09:48:27 +0200

    add rabbitmq consumer

---
+++ components/messaging/rabbitmq/rabbitmq-consumer/src/main/java/com/xpadro/rabbitmq/consumer/RabbitmqProducerApplication.java
@@ -0,0 +1,13 @@
+package com.xpadro.rabbitmq.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqProducerApplication.class, args);
+	}
+
+}
